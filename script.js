24883af0efee9c9f3565a651607e367130820fee
DISCOUNTS-FROM-ALI <dyntchev@gmail.com>
2025-01-19 23:57:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const openSiteButton = document.getElementById('openSiteButton');
 openSiteButton.addEventListener('click', () => {
     tg.openLink('https://sites.google.com/view/discounts-from-ali/');
 });
+const tg = window.Telegram.WebApp;
+
+if (!tg.initData) {
+    console.error("Web App не запущений в Telegram. Відкрийте його через бот.");
+} else {
+    console.log("Web App запущений в Telegram!");
+}
 
 // Автоматично розширюємо Web App на весь екран
 tg.expand();
